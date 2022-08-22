094489e90f418ea5810d680009e14537cfbf4f0d
Vladislav Tupikin <mrrefactoring@yandex.ru>
2022-08-22 15:22:57 +0300

    render improvements

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
 function drawLine(from, to) {
-  const horizontalDistance = Math.max(from.offsetLeft + from.offsetWidth, to.offsetLeft) - Math.min(from.offsetLeft + from.offsetWidth, to.offsetLeft);
-  const verticalDistance = Math.max(from.offsetTop, to.offsetTop) - Math.min(from.offsetTop, to.offsetTop);
-
   const startPointTopOffset = from.offsetTop + from.offsetHeight / 2;
   const startPointLeftOffset = from.offsetLeft + from.offsetWidth;
 
   const endPointTopOffset = to.offsetTop + to.offsetHeight / 2;
   const endPointLeftOffset = to.offsetLeft + to.offsetWidth;
+
+  const horizontalDistance = Math.max(from.offsetLeft + from.offsetWidth, to.offsetLeft) - Math.min(from.offsetLeft + from.offsetWidth, to.offsetLeft);
+  const verticalDistance = Math.floor(Math.max(startPointTopOffset, endPointTopOffset) - Math.min(startPointTopOffset, endPointTopOffset));
 
   const scale = window.devicePixelRatio;
   const lineWidth = 2;
@@ -18,24 +18,34 @@ function drawLine(from, to) {
   canvas.style.top = `${Math.min(startPointTopOffset, endPointTopOffset)}px`;
   canvas.style.left = `${Math.min(startPointLeftOffset, endPointLeftOffset)}px`;
   canvas.style.width = `${horizontalDistance}px`;
-  canvas.style.height = `${verticalDistance || 4}px`;
+  canvas.style.height = `${verticalDistance ? verticalDistance : 4}px`;
   canvas.width = Math.floor(horizontalDistance * scale);
   canvas.height = Math.floor((verticalDistance ? verticalDistance + lineWidth + 1 : lineWidth * 2) * scale);
 
   ctx.lineWidth = lineWidth;
   ctx.lineCap = 'round';
   ctx.lineJoin = 'round';
-  ctx.strokeStyle = '#ff5722';
+  ctx.strokeStyle = '#FF9800';
   ctx.scale(scale, scale);
 
   if (!verticalDistance) {
     ctx.moveTo(0, lineWidth);
     ctx.lineTo(horizontalDistance, lineWidth);
-  } else {
-    const direction = from.offsetTop < to.offsetTop ? 'down' : 'up';
+  } else if (verticalDistance < horizontalDistance) {
+    const direction = from.offsetTop + from.offsetHeight < to.offsetTop + to.offsetHeight ? 'down' : 'up';
 
     if (direction === 'down') {
-      ctx.bezierCurveTo(0, lineWidth, horizontalDistance / 2, lineWidth, horizontalDistance / 2, horizontalDistance /2);
+      ctx.bezierCurveTo(0, lineWidth, horizontalDistance / 2, lineWidth, horizontalDistance / 2, verticalDistance / 2);
+      ctx.bezierCurveTo(horizontalDistance / 2, verticalDistance / 2, horizontalDistance / 2, verticalDistance + lineWidth, horizontalDistance, verticalDistance + lineWidth)
+    } else {
+      ctx.bezierCurveTo(0, verticalDistance + lineWidth, horizontalDistance / 2, verticalDistance + lineWidth, horizontalDistance / 2, verticalDistance / 2);
+      ctx.bezierCurveTo(horizontalDistance / 2, verticalDistance / 2, horizontalDistance / 2, lineWidth, horizontalDistance, lineWidth);
+    }
+  } else {
+    const direction = from.offsetTop + from.offsetHeight < to.offsetTop + to.offsetHeight ? 'down' : 'up';
+
+    if (direction === 'down') {
+      ctx.bezierCurveTo(0, lineWidth, horizontalDistance / 2, lineWidth, horizontalDistance / 2, horizontalDistance / 2);
       ctx.lineTo(horizontalDistance / 2, verticalDistance - horizontalDistance / 2);
       ctx.bezierCurveTo(horizontalDistance / 2, verticalDistance - horizontalDistance / 2, horizontalDistance / 2, verticalDistance, horizontalDistance, verticalDistance + lineWidth);
     } else {
@@ -62,17 +72,25 @@ function drawLine(from, to) {
 }
 
 const links = [
-  { from: 'todo_1', to: 'inprogress_2' },
-  { from: 'inprogress_3', to: 'done_1' },
-  { from: 'todo_1', to: 'inprogress_4' },
-  { from: 'todo_3', to: 'inprogress_4' },
+  { from: 'todo_2', to: 'inprogress_3' },
+  { from: 'todo_2', to: 'inprogress_2' },
   { from: 'inprogress_2', to: 'done_1' },
-  { from: 'todo_5', to: 'inprogress_5' },
+  { from: 'inprogress_3', to: 'done_1' },
+  { from: 'todo_5', to: 'inprogress_4' },
+  { from: 'todo_4', to: 'inprogress_4' },
 ];
 
-for (const link of links) {
-  const fromElement = document.querySelector(`#${link.from}`);
-  const toElement = document.querySelector(`#${link.to}`);
+function render() {
+  document.querySelectorAll(`canvas`).forEach(element => element.remove());
 
-  drawLine(fromElement, toElement);
+  for (const link of links) {
+    const fromElement = document.querySelector(`#${link.from}`);
+    const toElement = document.querySelector(`#${link.to}`);
+
+    drawLine(fromElement, toElement);
+  }
 }
+
+window.addEventListener('resize', render);
+
+render();
