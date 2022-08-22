1d21e59e00bcaa3b5f63cd50d753abb8b33bf511
Vladislav Tupikin <mrrefactoring@yandex.ru>
2022-08-22 12:43:21 +0300

    new line drawing methods. vertical line draw fixes

--- src/script.js
+++ src/script.js
@@ -11,20 +11,36 @@ function drawLine(from, to) {
   const canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
 
+  const lineWidth = 2;
+
   canvas.style.position = 'absolute';
   canvas.style.top = `${Math.min(startPointTop, endPointTop)}px`;
   canvas.style.left = `${Math.min(startPointLeft, endPointLeft)}px`;
   canvas.width = horizontalDistance;
-  canvas.height = verticalDistance || 2;
-  canvas.style.zIndex = '2122';
+  canvas.height = verticalDistance ? verticalDistance + lineWidth + 1 : lineWidth * 2;
 
-  ctx.lineWidth = 2;
+  ctx.lineWidth = lineWidth;
   ctx.lineCap = 'round';
   ctx.lineJoin = 'round';
-  ctx.moveTo(0, 0);
-  ctx.bezierCurveTo(0, 0, horizontalDistance / 2, 0, horizontalDistance / 2, verticalDistance / 2);
-  ctx.bezierCurveTo(horizontalDistance / 2, verticalDistance / 2, horizontalDistance / 2, verticalDistance, horizontalDistance, verticalDistance);
   ctx.strokeStyle = '#ff5722';
+
+  if (!verticalDistance) {
+    ctx.moveTo(0, lineWidth);
+    ctx.lineTo(horizontalDistance, lineWidth);
+  } else {
+    const direction = from.offsetTop < to.offsetTop ? 'down' : 'up';
+
+    if (direction === 'down') {
+      ctx.bezierCurveTo(0, lineWidth, horizontalDistance / 2, lineWidth, horizontalDistance / 2, horizontalDistance /2);
+      ctx.lineTo(horizontalDistance / 2, verticalDistance - horizontalDistance / 2);
+      ctx.bezierCurveTo(horizontalDistance / 2, verticalDistance - horizontalDistance / 2, horizontalDistance / 2, verticalDistance, horizontalDistance, verticalDistance + lineWidth);
+    } else {
+      ctx.bezierCurveTo(0, verticalDistance + lineWidth, horizontalDistance / 2, verticalDistance + lineWidth, horizontalDistance / 2, verticalDistance - horizontalDistance / 2);
+      ctx.lineTo(horizontalDistance / 2, horizontalDistance / 2);
+      ctx.bezierCurveTo(horizontalDistance / 2, horizontalDistance / 2, horizontalDistance / 2, lineWidth, horizontalDistance, lineWidth);
+    }
+  }
+
   ctx.stroke();
 
   document.body.appendChild(canvas);
@@ -43,8 +59,11 @@ function drawLine(from, to) {
 
 const links = [
   { from: 'todo_1', to: 'inprogress_2' },
-  { from: 'todo_3', to: 'inprogress_3' },
-  // { from: 'inprogress_3', to: 'done_1' },
+  { from: 'inprogress_3', to: 'done_1' },
+  { from: 'todo_1', to: 'inprogress_4' },
+  { from: 'todo_3', to: 'inprogress_4' },
+  { from: 'inprogress_2', to: 'done_1' },
+  { from: 'todo_5', to: 'inprogress_5' },
 ];
 
 for (const link of links) {
